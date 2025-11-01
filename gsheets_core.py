1ee89a980a7cee4f7e4a37162e039afb85d10d66
lythaoanh0103-pixel <lythaoanh0103@gmail.com>
2025-11-01 23:11:49 +0700

    Update gsheets_core.py

--- gsheets_core.py
+++ gsheets_core.py
@@ -1,3 +1,11 @@
+import time
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import streamlit as st
+import pandas as pd
+from typing import List, Callable
+SHEET_ID = "1icpLUH3UNvMKuoB_hdiCTiwZ-tbY9aPJEOHGSfBWECY"
+
 @st.cache_resource
 def gs_client() -> gspread.client.Client:
     scope = [
@@ -15,3 +23,4 @@ def gs_client() -> gspread.client.Client:
         creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 
     return gspread.authorize(creds)
+
